6546f179a56b543a91b97ee8f3743760b278d27e
ParadoxTwo <winbro2.ej@gmail.com>
2021-09-11 01:00:56 +1000

    Setup Staging

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Website Under Development
+          Website Under Development (Staging)
         </p>
         <a
           className="App-link"
